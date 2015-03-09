f9d9e4e8471c39963aa15c86de627e767b6350d4
Ryan Spittler <ryanspittler@gmail.com>
2015-03-08 19:04:26 -0600

    Add total, purchase and payment methods to CashRegister class
    - All three return the total with the cents in proper format.
    - Change will be listed if the payment equals or exceeds the total.
    
    [#89843784]

--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -2,7 +2,35 @@ require 'rspec'
 require_relative('../../lib/cash_register')
 
 describe CashRegister do
-  it 'should be a cash register' do
-    expect(true).to eq(true)
+  let(:cashregister) { CashRegister.new }
+  it 'can be created' do
+    expect(cashregister).to be_a(CashRegister)
+  end
+
+  it 'can give the total' do
+    expect(cashregister.total).to eq('Your total is $0.00.')
+  end
+
+  it 'can add purchases' do
+    cashregister.purchase(5.0)
+    expect(cashregister.total).to eq('Your total is $5.00.')
+  end
+
+  context 'for payments' do
+    context 'when the payment is less than the purchase' do
+      it 'should return the remaining outstanding amount' do
+        cashregister.purchase(10.0)
+        expect(cashregister.payment(6.0)).to eq('Your new total is $4.00.')
+        expect(cashregister.total).to eq('Your total is $4.00.')
+      end
+    end
+
+    context 'when the payment is equal or more than the purchase' do
+      it 'should return the change' do
+        cashregister.purchase(5.0)
+        expect(cashregister.payment(6.0)).to eq('Your change is $1.00.')
+        expect(cashregister.total).to eq('Your total is $0.00.')
+      end
+    end
   end
 end
